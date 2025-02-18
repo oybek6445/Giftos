622a52b7f1fcc8d6f149de02ae5ad0e1306f1c3d
Lazizbek <azimovl566@gmail.com>
2025-02-18 10:32:54 +0500

    laziz

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
-const env = require('dotenv').config()
-const app  = require("./middleware/app.js")
-const ejs = require('ejs');
-app.listen(process.env.PORT, ()=>{
-    console.log(`Server ${process.env.PORT} da ishga tushdi`)
-})
+const env = require("dotenv").config();
+const app = require("./middleware/app.js");
+const ejs = require("ejs");
+app.listen(9000);
